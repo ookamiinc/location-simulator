8f80d00d1ce9e2f49406ef72a3d4c09962ac39dc
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-05-21 15:20:56 +0900

    Use _ to indicate that it won't be used

--- simulate.rb
+++ simulate.rb
@@ -36,8 +36,8 @@ def distance(loc1, loc2)
   dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg # Delta, converted to rad
   dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg
 
-  lat1_rad, lon1_rad = loc1.map { |i| i * rad_per_deg }
-  lat2_rad, lon2_rad = loc2.map { |i| i * rad_per_deg }
+  lat1_rad, _lon1_rad = loc1.map { |i| i * rad_per_deg }
+  lat2_rad, _lon2_rad = loc2.map { |i| i * rad_per_deg }
 
   a = Math.sin(dlat_rad / 2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad / 2)**2
   c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
