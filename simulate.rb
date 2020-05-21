d27177dfd6083eaf70bb09e9a5f9bb196c17e6bd
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-05-21 15:15:32 +0900

    Run rubocop -a

--- simulate.rb
+++ simulate.rb
@@ -1,7 +1,7 @@
 require 'json'
 require 'byebug'
 
-GEOJSON_FILE = 'okayama.geojson'
+GEOJSON_FILE = 'okayama.geojson'.freeze
 
 def simulate
   last_lat = nil
@@ -31,18 +31,18 @@ def coordinates_from_geojson
 end
 
 def distance(loc1, loc2)
-  rad_per_deg = Math::PI/180  # PI / 180
+  rad_per_deg = Math::PI / 180 # PI / 180
   rkm = 6371                  # Earth radius in kilometers
   rm = rkm * 1000             # Radius in meters
 
-  dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
-  dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+  dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg # Delta, converted to rad
+  dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg
 
-  lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
-  lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+  lat1_rad, lon1_rad = loc1.map { |i| i * rad_per_deg }
+  lat2_rad, lon2_rad = loc2.map { |i| i * rad_per_deg }
 
-  a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
-  c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+  a = Math.sin(dlat_rad / 2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad / 2)**2
+  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
   rm * c # Delta in meters
 end
